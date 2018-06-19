4e2d245b8027d4de8d5d400abd07a63a4651dd4d
Горин Александр <pu1s@outlook.com>
2018-06-19 18:02:29 +0400

    ++

--- asterix/main.cpp
+++ asterix/main.cpp
@@ -2,23 +2,25 @@
 #include <iostream>
 
 #include <lib_asterix.h>
+#include "main.h"
 using namespace std;
 
 int main()
 {
 	asterix_footer();
-	asx::_formatter f((unsigned char)asx::_frmt_param::FRMT_PARAM_DEFAULT | (unsigned char)asx::_frmt_param::FRMT_PARAM_SHOWPOS, 5);
-	auto fp((unsigned char)asx::_frmt_param::FRMT_PARAM_SHOWPOS | (unsigned char)asx::_frmt_param::FRMT_PARAM_SHOWEXP);
-	if (fp & (unsigned char)asx::_frmt_param::FRMT_PARAM_SHOWPOS) {
-		cout << "WOW" << endl;
-	} ;
-
-	asx::types::basic_point::formatter fyyy((unsigned char)asx::_frmt_param::FRMT_PARAM_SHOWPOS);
-	asx::types::basic_point aut;
-	aut.format.set_format_parameters((unsigned char)asx::_frmt_param::FRMT_PARAM_SHOWPOS);
-	cout << aut.to_string() << endl;
+	NewFunction();
 #ifdef _WIN32
 	system("pause");
 #endif
 	return 0;
+}
+
+void NewFunction()
+{
+	asx::_formatter f((unsigned char)asx::_frmt_param::FRMT_PARAM_DEFAULT | (unsigned char)asx::_frmt_param::FRMT_PARAM_SHOWPOS, 5);
+	
+	/*asx::types::basic_point::formatter fyyy((unsigned char)asx::_frmt_param::FRMT_PARAM_SHOWPOS);
+	asx::types::basic_point aut;
+	aut.format.set_format_parameters((unsigned char)asx::_frmt_param::FRMT_PARAM_SHOWPOS);
+	cout << aut.to_string() << endl;*/
 }
