71e56b913808705a8c58365e59dee8546d74e6bc
pu1s <pu1s@outlook.com>
2018-06-27 11:04:05 +0400

    /*\

--- asterix/main.cpp
+++ asterix/main.cpp
@@ -17,12 +17,6 @@ int main()
 
 void NewFunction()
 {
-	asx::_formatter f((unsigned char)asx::FRMT_CUSTOM_PARAM::FRMT_PARAM_DEFAULT | (unsigned char)asx::FRMT_CUSTOM_PARAM::FRMT_PARAM_SHOWPOS, 5);
+	asx::_custom_formatter f((unsigned char)asx::FRMT_CUSTOM_PARAM::FRMT_PARAM_DEFAULT | (unsigned char)asx::FRMT_CUSTOM_PARAM::FRMT_PARAM_SHOWPOS, 5);
 	
-	asx::types::basic_point_item::formatter fyyy((unsigned char)asx::FRMT_CUSTOM_PARAM::FRMT_PARAM_SHOWPOS);
-	asx::types::point_item<int> aut;
-	asx::types::point_item<int> ttt(aut);
-	auto ff = fyyy.get_format_parameters();
-	ttt = aut;
-	cout << aut.to_string() << endl;
 }
